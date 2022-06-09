46b6717f62e806e8a6cda782f497645e8cc97414
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-06-09 13:46:19 -0400

    commit tree b7c4d6456476c6482cc1de99ce48b3399f9d801a

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        })
+    })
+    .then(resp => resp.json())
+    .then(json => {
+        const h1 = document.createElement("h1");
+        h1.textContent = `ID: ${json.id}`;
+        document.body.append(h1);
+    })
+    .catch(error => {
+        const h1 = document.createElement("h1");
+        h1.textContent = `ERROR: ${error.message}`;
+        document.body.append(h1);
+    });
+}
